6b28ebb55404bc2b367a12e2f62db269575ab471
Aleksey Profatilov <snakeduse@gmail.com>
2015-11-12 12:08:20 +0300

    сделал кастомный диалог создания карточки

--- src/Tasks/Scripts/Constrollers/tasksController.js
+++ src/Tasks/Scripts/Constrollers/tasksController.js
@@ -10,20 +10,31 @@
     function tasksController($scope, Tasks, $mdDialog) {
         $scope.Tasks = Tasks.query();
 
-        $scope.showAlert = function (ev) {
-            var confirm = $mdDialog.confirm()
-                  .title('Create new card')
-                  .content('All of the banks have agreed to <span class="debt-be-gone">forgive</span> you your debts.')
-                  .ariaLabel('Lucky day')
-                  .targetEvent(ev)
-                  .ok('Create')
-                  .cancel('Cancel');
-
-            $mdDialog.show(confirm).then(function () {
-                $scope.status = 'You decided to get rid of your debt.';
-            }, function () {
-                $scope.status = 'You decided to keep your debt.';
-            });
+        var DialogController = function ($scope, $mdDialog) {
+            $scope.hide = function () {
+                $mdDialog.hide();
+            };
+            $scope.cancel = function () {
+                $mdDialog.cancel();
+            };
+            $scope.answer = function (answer) {
+                $mdDialog.hide(answer);
+            };
         };
+
+        $scope.showAlert = function (ev) {
+            $mdDialog.show({
+                //controller: DialogController,
+                templateUrl: 'create_dialog.tmpl.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true
+            })
+            .then(function (answer) {
+                $scope.status = 'You said the information was "' + answer + '".';
+            }, function () {
+                $scope.status = 'You cancelled the dialog.';
+            });
+        };        
     }
 })();
