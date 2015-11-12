0de1c4b560a3ccdf421629b8a531b95381001154
Aleksey Profatilov <snakeduse@gmail.com>
2015-11-12 23:22:11 +0300

    Создал модель карточки, контекст базы данных и добавил EF 7 в зависимости

---
+++ src/Tasks/Infrastructure/TasksContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tasks.Infrastructure
+{
+    public class TasksContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
---
+++ src/Tasks/Models/Card.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tasks.Models
+{
+    /// <summary>
+    /// Модель карточки
+    /// </summary>
+    public class Card
+    {
+        /// <summary>
+        /// Идентификатор карточки
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Заголовок карточки
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Находится ли карточка в архиве
+        /// </summary>
+        public bool IsInArchive { get; set; }
+
+        /// <summary>
+        /// Задачи карточки
+        /// </summary>
+        public List<Task> Tasks { get; set; }
+    }
+}
