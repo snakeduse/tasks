115174551e3821b553fb5d4fd1268d3929cd8638
Aleksey Profatilov <snakeduse@gmail.com>
2015-11-12 11:48:25 +0300

    добавил диалог подтверждения

--- src/Tasks/Scripts/Constrollers/tasksController.js
+++ src/Tasks/Scripts/Constrollers/tasksController.js
@@ -5,22 +5,25 @@
         .module('tasksApp')
         .controller('tasksController', tasksController);
 
-    tasksController.$inject = ['$scope', 'Tasks', '$mdDialog']; 
+    tasksController.$inject = ['$scope', 'Tasks', '$mdDialog'];
 
     function tasksController($scope, Tasks, $mdDialog) {
         $scope.Tasks = Tasks.query();
 
         $scope.showAlert = function (ev) {
-            $mdDialog.show(
-              $mdDialog.alert()
-                .parent(angular.element(document.querySelector('#popupContainer')))
-                .clickOutsideToClose(true)
-                .title('Create new card')
-                .content('You can specify some description text in here.')
-                .ariaLabel('Alert Dialog Demo')
-                .ok('Create')
-                .targetEvent(ev)
-            );
+            var confirm = $mdDialog.confirm()
+                  .title('Create new card')
+                  .content('All of the banks have agreed to <span class="debt-be-gone">forgive</span> you your debts.')
+                  .ariaLabel('Lucky day')
+                  .targetEvent(ev)
+                  .ok('Create')
+                  .cancel('Cancel');
+
+            $mdDialog.show(confirm).then(function () {
+                $scope.status = 'You decided to get rid of your debt.';
+            }, function () {
+                $scope.status = 'You decided to keep your debt.';
+            });
         };
     }
 })();
