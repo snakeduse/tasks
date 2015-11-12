3e26169988d59f95fbd5d5bd3e9c7cf1bcb26c36
Aleksey Profatilov <snakeduse@gmail.com>
2015-11-12 11:40:28 +0300

    Привязал кнопку создания карты к диалогу

--- src/Tasks/Scripts/Constrollers/tasksController.js
+++ src/Tasks/Scripts/Constrollers/tasksController.js
@@ -5,9 +5,22 @@
         .module('tasksApp')
         .controller('tasksController', tasksController);
 
-    tasksController.$inject = ['$scope', 'Tasks']; 
+    tasksController.$inject = ['$scope', 'Tasks', '$mdDialog']; 
 
-    function tasksController($scope, Tasks) {
+    function tasksController($scope, Tasks, $mdDialog) {
         $scope.Tasks = Tasks.query();
+
+        $scope.showAlert = function (ev) {
+            $mdDialog.show(
+              $mdDialog.alert()
+                .parent(angular.element(document.querySelector('#popupContainer')))
+                .clickOutsideToClose(true)
+                .title('Create new card')
+                .content('You can specify some description text in here.')
+                .ariaLabel('Alert Dialog Demo')
+                .ok('Create')
+                .targetEvent(ev)
+            );
+        };
     }
 })();
